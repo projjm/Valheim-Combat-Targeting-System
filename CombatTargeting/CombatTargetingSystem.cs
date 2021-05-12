34811ec1fb16c94abb81b7f748b602cfebfacc65
projjm <projjmail@gmail.com>
2021-05-12 23:35:33 +0100

    Updated mod version

--- CombatTargeting/CombatTargetingSystem.cs
+++ CombatTargeting/CombatTargetingSystem.cs
@@ -56,7 +56,7 @@ namespace CombatTargetingSystem
         public float w_targetIsDamaged;
     }
 
-    [BepInPlugin("projjm.combattargetingsystem", "Combat Targeting System", "1.1.0")]
+    [BepInPlugin("projjm.combattargetingsystem", "Combat Targeting System", "1.1.1")]
     [BepInProcess("valheim.exe")]
 
     public class CombatTargetingSystem : BaseUnityPlugin
